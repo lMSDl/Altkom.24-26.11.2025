ba50b1e13c0191f9aae2a54c02db5ede04db2be7
Paweł <pawel@msd.info.pl>
2025-11-24 13:28:23 +0100

    ISP

---
+++ SOLID/ISP.cs
@@ -0,0 +1,43 @@
+﻿namespace SOLID
+{
+
+    /*public interface IFormatter
+    {
+        void ToPdf();
+        void ToExcel();
+    }*/
+
+    public interface IPdfFormatter
+    {
+        void ToPdf();
+    }
+
+    public interface IExcelFormatter
+    {
+        void ToExcel();
+    }
+
+    public class Report : IPdfFormatter, IExcelFormatter
+    {
+        public void ToPdf()
+        {
+            Console.WriteLine("Report to PDF");
+        }
+        public void ToExcel()
+        {
+            Console.WriteLine("Report to Excel");
+        }
+    }
+
+    public class Poem : IPdfFormatter
+    {
+        public void ToPdf()
+        {
+            Console.WriteLine("Poem to PDF");
+        }
+        /*public void ToExcel()
+        {
+            
+        }*/
+    }
+}
