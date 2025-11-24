72455cd05cb0c2331b377507a1407b2d526983c0
Paweł <pawel@msd.info.pl>
2025-11-24 14:31:48 +0100

    Builder - before

---
+++ DesignPatterns/Creational/Builder/Client.cs
@@ -0,0 +1,13 @@
+﻿namespace DesignPatterns.Creational.Builder
+{
+    internal class Client
+    {
+        public static void Execute()
+        {
+            var vehicle = new Vehicle(4, 5, 4, 500, 100);
+            vehicle = new Vehicle(wheels: 4, seats: 5, doors: 4, trunkCapacity: 500, enginePower: 100);
+
+            Console.WriteLine(vehicle);
+        }
+    }
+}
