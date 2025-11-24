ba6ccf67b800f732622168e01f6220cf674f5e2d
Paweł <pawel@msd.info.pl>
2025-11-24 13:42:04 +0100

    DIP

---
+++ SOLID/DIP.cs
@@ -0,0 +1,66 @@
+﻿namespace SOLID
+{
+    public interface IMessage
+    {
+        void Send();
+    }
+
+    public class SMS : IMessage
+    {
+        public string PhoneNumber { get; set; }
+        public string Content { get; set; }
+
+        public void Send()
+        {
+            Console.WriteLine($"Sending SMS to {PhoneNumber}: {Content}");
+        }
+    }
+
+    public class MMS : IMessage
+    {
+        public string PhoneNumber { get; set; }
+        public byte[] Content { get; set; }
+
+        public void Send()
+        {
+            Console.WriteLine($"Sending MMS to {PhoneNumber}: {Content.Length} bytes");
+        }
+    }
+
+    public class Email : IMessage
+    {
+        public string EmailAddress { get; set; }
+        public string Subject { get; set; }
+        public string Content { get; set; }
+        public void Send()
+        {
+            Console.WriteLine($"Sending Email to {EmailAddress}: {Subject} - {Content}");
+        }
+    }
+
+
+    public class Messenger
+    {
+        public IEnumerable<IMessage> Messages { get; set; }
+
+        public Messenger(IEnumerable<IMessage> messages)
+        {
+            Messages = messages;
+        }
+
+        public void SendMessages(IEnumerable<IMessage> messages)
+        {
+            Messages = messages;
+            SendMessages();
+        }
+
+        public void SendMessages()
+        {
+            foreach (var message in Messages)
+            {
+                message.Send();
+            }
+        }
+
+    }
+}
