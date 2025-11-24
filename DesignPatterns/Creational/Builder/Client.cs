3122367e5d1d230eab278ab878b30e0f0ebfb392
Paweł <pawel@msd.info.pl>
2025-11-24 14:46:45 +0100

    Builder - after

--- DesignPatterns/Creational/Builder/Client.cs
+++ DesignPatterns/Creational/Builder/Client.cs
@@ -7,6 +7,23 @@
             var vehicle = new Vehicle(4, 5, 4, 500, 100);
             vehicle = new Vehicle(wheels: 4, seats: 5, doors: 4, trunkCapacity: 500, enginePower: 100);
 
+            //var vehicleBuilder = new Vehicle.VehicleBuilder().Build();
+
+            Console.WriteLine(vehicle);
+
+
+            var vehicleBuilder = new VehicleBuilder();
+            vehicleBuilder.SetWheels(4);
+            vehicleBuilder.SetSeats(5);
+            vehicleBuilder.SetDoors(4);
+            vehicleBuilder.SetTrunkCapacity(500);
+            vehicleBuilder.SetEnginePower(100);
+            vehicle = vehicleBuilder.Build();
+
+            Console.WriteLine(vehicle);
+
+
+            vehicle = new Vehicle() { Wheels = 4, Seats = 5, TrunkCapacity = 500, Doors = 4, EnginePower = 100 };
             Console.WriteLine(vehicle);
         }
     }
--- DesignPatterns/Creational/Builder/Vehicle.cs
+++ DesignPatterns/Creational/Builder/Vehicle.cs
@@ -2,28 +2,49 @@
 {
     internal class Vehicle
     {
+        // Builder często występuje jako klasa zagnieżdżona
+        // tutaj pokazano szkic implementacji wzorca Builder
+        // wymaga on zablokowania konstruktora klasy Vehicle (private), dzięki czemu builder jest jedynym sposobem tworzenia obiektów tej klasy
+        /*public class VehicleBuilder()
+        {
+            private Vehicle _vehicle = new Vehicle();
+
+            public Vehicle Build()
+            {
+                return _vehicle;
+            }
+        }
+
+        private Vehicle()
+        {
+        }*/
+
+        public Vehicle()
+        {
+        }
+
         public Vehicle(int wheels, int seats)
-        {
-            Wheels = wheels;
-            Seats = seats;
-        }
+         {
+             Wheels = wheels;
+             Seats = seats;
+         }
 
-        public Vehicle(int wheels, int seats, int? trunkCapacity) : this(wheels, seats)
-        {
-            TrunkCapacity = trunkCapacity;
-        }
+         public Vehicle(int wheels, int seats, int? trunkCapacity) : this(wheels, seats)
+         {
+             TrunkCapacity = trunkCapacity;
+         }
 
-        public Vehicle(int wheels, int? enginePower, int seats) : this(wheels, seats)
-        {
-            EnginePower = enginePower;
-        }
+         public Vehicle(int wheels, int? enginePower, int seats) : this(wheels, seats)
+         {
+             EnginePower = enginePower;
+         }
 
-        public Vehicle(int wheels, int seats, int doors, int? trunkCapacity, int? enginePower) : this(wheels, seats)
-        {
-            Doors = doors;
-            TrunkCapacity = trunkCapacity;
-            EnginePower = enginePower;
-        }
+         public Vehicle(int wheels, int seats, int doors, int? trunkCapacity, int? enginePower) : this(wheels, seats)
+         {
+             Doors = doors;
+             TrunkCapacity = trunkCapacity;
+             EnginePower = enginePower;
+         }
 
 
         public int Wheels { get; set; }
