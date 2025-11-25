eaa70c881c35f8b465ba760a9aca0847abffd990
Paweł <pawel@msd.info.pl>
2025-11-25 14:56:39 +0100

    Proxy - before

---
+++ DesignPatterns/Structural/Proxy/Client.cs
@@ -0,0 +1,27 @@
+﻿namespace DesignPatterns.Structural.Proxy
+{
+    internal class Client
+    {
+        public static void Execute()
+        {
+            IDatabase database = new Database();
+
+            for (int i = 1; i < 11; i++)
+            {
+                Thread.Sleep(250);
+                _ = database.RequestAsync(i);
+            }
+
+            Console.ReadLine();
+
+            for (int i = 1; i < 11; i++)
+            {
+                _ = database.RequestAsync(i);
+            }
+
+            Console.ReadLine();
+
+            database.Dispose();
+        }
+    }
+}
