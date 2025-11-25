23d92235e063f259dcdcb7e73434074597231b14
Paweł <pawel@msd.info.pl>
2025-11-25 12:48:03 +0100

    Decorator - before

---
+++ DesignPatterns/Structural/Decorator/Client.cs
@@ -0,0 +1,16 @@
+﻿namespace DesignPatterns.Structural.Decorator
+{
+    internal class Client
+    {
+        public static void Execute()
+        {
+            while (true)
+            {
+                var text = Console.ReadLine();
+                INotification notification = new SystemNotification();
+
+                notification.Send(text);
+            }
+        }
+    }
+}
