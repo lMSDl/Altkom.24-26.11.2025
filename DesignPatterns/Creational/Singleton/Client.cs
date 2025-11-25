d23dca9fb11397dede4fbb8358cd90a70f2f9a98
Paweł <pawel@msd.info.pl>
2025-11-25 10:09:45 +0100

    Singleton

---
+++ DesignPatterns/Creational/Singleton/Client.cs
@@ -0,0 +1,30 @@
+﻿namespace DesignPatterns.Creational.Singleton
+{
+    internal class Client
+    {
+        public static void Execute()
+        {
+            Task.Run(() =>
+            {
+                var service1 = Context.Instance;
+                Console.WriteLine(service1.GetSettings("1"));
+            });
+
+            Task.Run(() =>
+            {
+                var service2 = Context.Instance;
+                Console.WriteLine(service2.GetSettings("2"));
+                service2.SetSettings("3", "x");
+            });
+
+            Task.Run(() =>
+            {
+                var service3 = Context.Instance;
+                Console.WriteLine(service3.GetSettings("3"));
+            });
+
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ DesignPatterns/Creational/Singleton/Context.cs
@@ -0,0 +1,69 @@
+﻿namespace DesignPatterns.Creational.Singleton
+{
+    internal class Context
+    {
+        private readonly Dictionary<string, string> _settings = new Dictionary<string, string>();
+        private Context()
+        {
+            Console.WriteLine("Initializing Context");
+            _settings["1"] = "a";
+            _settings["2"] = "b";
+            _settings["3"] = "c";
+        }
+
+        public string GetSettings(string key)
+        {
+            return _settings[key];
+        }
+        public void SetSettings(string key, string value)
+        {
+            _settings[key] = value;
+        }
+
+        private static Context? _instance;
+        /*public static Context GetInstance()
+        {
+            if (_instance is null)
+                _instance = new Context();
+            return _instance;
+        }*/
+
+        private static readonly Lock _instanceLock = new();
+        /*public static Context GetInstance()
+        {
+            lock (_instanceLock)
+            {
+                if (_instance is null)
+                    _instance = new Context();
+                return _instance;
+            }
+        }*/
+
+        /*public static Context GetInstance()
+        {
+            if (_instance is null)
+            {
+                lock (_instanceLock)
+                {
+                    if (_instance is null)
+                        _instance = new Context();
+                }
+            }
+            return _instance;
+        }*/
+
+        //jawny statyczny konstruktor usuwa flagę "beforefieldinit"
+        //dzięku temu mamy gwarancję, że instancja zostanie utworzona w momencie pierwszego odwołania do klasy
+        static Context()
+        {
+        }
+        public static Context Instance { get; } = new Context();
+
+
+        private static readonly Lazy<Context> _lazyInstance = new Lazy<Context>(() => new Context());
+        public static Context GetInstance()
+        {
+            return _lazyInstance.Value;
+        }
+    }
+}
