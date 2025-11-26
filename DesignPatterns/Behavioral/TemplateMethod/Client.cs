b7c260542f40e66609afa089f0f2ccf0d8b42f0c
Paweł <pawel@msd.info.pl>
2025-11-26 13:29:16 +0100

    Template Method - before

---
+++ DesignPatterns/Behavioral/TemplateMethod/Client.cs
@@ -0,0 +1,17 @@
+﻿namespace DesignPatterns.Behavioral.TemplateMethod
+{
+    internal class Client
+    {
+        public static void Execute()
+        {
+            var fileLogger = new FileLogger();
+            fileLogger.Log("Message to Log in File.");
+            Console.WriteLine();
+            var emailLogger = new EmailLogger();
+            emailLogger.Log("Message to Log via Email.");
+            Console.WriteLine();
+            var databaseLogger = new DatabaseLogger();
+            databaseLogger.Log("Message to Log in DB.");
+        }
+    }
+}
---
+++ DesignPatterns/Behavioral/TemplateMethod/FileService.cs
@@ -0,0 +1,15 @@
+﻿namespace DesignPatterns.Behavioral.TemplateMethod
+{
+    internal class FileService : IDisposable
+    {
+        public void Dispose()
+        {
+            Console.WriteLine("Close File.");
+        }
+
+        public void Write(string @string)
+        {
+            Console.WriteLine("Appending Log message to file : " + @string);
+        }
+    }
+}
