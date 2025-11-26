a6486a8690d35749f8d4236d179995ffe3a50980
Paweł <pawel@msd.info.pl>
2025-11-26 14:46:33 +0100

    Null Object - before

---
+++ DesignPatterns/Behavioral/NullObject/Client.cs
@@ -0,0 +1,25 @@
+﻿namespace DesignPatterns.Behavioral.NullObject
+{
+    internal class Client
+    {
+        public static void Execute()
+        {
+            var objects = new AbstractObject[] { new RealObject(), null };
+
+            while (true)
+            {
+                var value = Random.Shared.Next(0, objects.Length);
+
+                Console.ReadLine();
+                var item = objects[value];
+
+                /*if (item == null)
+                    continue;*/
+
+                item?.Method1();
+                item?.Method2();
+                item?.Method3();
+            }
+        }
+    }
+}
