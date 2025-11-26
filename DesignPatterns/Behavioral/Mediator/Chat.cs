b1c6224a5836796ce2b48ed4dfeeceddb40f4b8c
Paweł <pawel@msd.info.pl>
2025-11-26 10:26:51 +0100

    Mediator

---
+++ DesignPatterns/Behavioral/Mediator/Chat.cs
@@ -0,0 +1,34 @@
+﻿namespace DesignPatterns.Behavioral.Mediator
+{
+    internal class Chat : IMediator
+    {
+        private readonly ICollection<ChatMemeber> _members = [];
+        public void Join(ChatMemeber member)
+        {
+            if (!_members.Contains(member))
+                _members.Add(member);
+        }
+
+        public void Quit(ChatMemeber member)
+        {
+            if (_members.Contains(member))
+                _members.Remove(member);
+        }
+
+        public void Send(ChatMemeber from, string message)
+        {
+            var query = _members.Where(x => from != x);
+            if (from is ChatBot)
+                query = query.Where(x => x is not ChatBot);
+            foreach (var member in query)
+            {
+                member.Receive(from.Nick, message, false);
+            }
+        }
+
+        public void Send(ChatMemeber from, string to, string message)
+        {
+            _members.FirstOrDefault(x => x.Nick == to)?.Receive(from.Nick, message, true);
+        }
+    }
+}
---
+++ DesignPatterns/Behavioral/Mediator/ChatUser.cs
@@ -0,0 +1,12 @@
+﻿namespace DesignPatterns.Behavioral.Mediator
+{
+    internal class ChatUser : ChatMemeber
+    {
+        public override void Receive(string from, string message, bool isPrivate)
+        {
+            Console.WriteLine(isPrivate
+                ? $"{from} (Private): {message}"
+                : $"{from}: {message}");
+        }
+    }
+}
